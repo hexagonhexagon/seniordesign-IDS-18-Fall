2d10e7fa7e32fd1ae14edfae6a69797c1c929d5a
Alari Maricq <amaricq@umich.edu>
2019-01-15 00:01:14 -0500

    [rules] added _load method and save_file property to Rule.
    save_file is a class property for determining profile save data
    location.
    _load is a method for loading that data into an instance.
    They are provided for convenience, but not required to be used by child
    classes.
    
    Made several edits to method docstrings

--- IDS/rule_abc.py
+++ IDS/rule_abc.py
@@ -1,23 +1,23 @@
 """Rule Abstract Base Class"""
 
 from abc import ABC, abstractmethod
+import json
 import pathlib
 
 
 class Rule(ABC):
     """Rule Abstract Base Class
-    This is an abstract class; it defines an interface for child classes to
-    implement. Each rule for use in the Rules Based IDS should derive from this
-    class. Only the “Test” function is required to be implemented by a child
-    class. If the rule function needs a set of working data for its heuristics,
-    then the "Prepare" function should be implemented.
+    This defines an interface for rules used in the Rules Based IDS.
+    Only the “test” function is required to be implemented by a child class.
+    If the rule function needs a set of working data for its heuristics,
+    then the "prepare" function should be implemented.
 
     Attributes:
         profile_id: string indicating the usage profile for the rule. For
         example, "Ford_Fusion_2018"
 
         Working data {multiple, various}: set of information used for an
-        algorithm’s operation.  These properties are to be private, defined by
+        algorithm’s operation.  These attributes are to be private, defined by
         each child class as needed. As such, this abstract class will not
         provide declarations for these items.
     """
@@ -25,23 +25,34 @@ class Rule(ABC):
     # use this file as path reference
     # MAYBE: make a file in project root to define global constants, such as
     # this.
-    SAVE_PATH = pathlib.Path(__file__).parent.parent / 'savedata/rule-data'
+    SAVE_PATH = pathlib.Path(__file__).parent.parent / 'savedata/rule-profiles'
 
     def __init__(self, profile_id):
-        # Define attributes all rules should have
-        if not profile_id:
-            raise ValueError("profile_id can not be empty")
+        """Init Rule
+        Raises:
+            ValueError: if profile_id is not a valid python identifier.
+        """
+        if not profile_id.is_identifier():
+            raise ValueError("profile_id needs to be valid python identifier")
         self.profile_id = profile_id
-        # Initialize super-class
         super().__init__()
+
+    @property
+    def save_file(self):
+        """Define profile save path"""
+        # self.__class__.__name__ provides the name of the current instance.
+        # This will be the name of the child class.
+        return self.SAVE_PATH / self.profile_id / '{}.json'.format(
+            self.__class__.__name__)
 
     @abstractmethod
     def test(self, canlist):
         """Classify CAN packets
         A function that takes a list of one or more CAN frames, and use a rule
-        to classify them. This function will be referenced
-        by the “Test Frame” and “Test Series” functions of the Rules IDS
-        class.
+        to classify them. This function will be referenced by the “Test Frame”
+        and “Test Series” functions of the Rules IDS class.
+        If any working data is needed, it should be defined by implementing the
+        "prepare" method.
 
         Returns: [ bool, ... ]
             The classification is a list of boolean values. The function should
@@ -54,26 +65,50 @@ class Rule(ABC):
         pass
 
     def prepare(self, canlist=None):
-        """
+        """Prepare rule heuristics
         This method should be implemented as needed.
-        Accepts a list of CAN frames and a string identifier for the vehicle
-        profile. Analyze a list of CAN packets to provide working data for a
-        corresponding Rule Function.
+
+        Analyze a list of CAN packets to provide working data for the test
+        method.
+        If CAN data is not provided, existing working data should be loaded
+        from save files. The _load method can be used for this.
+
         Working data should be saved to a JSON file named after the class
-        writing it (e.g. Whitelist.json), in a subfolder of the “savedata”
-        directory, corresponding to the vehicle profile (e.g. Ford_Fusion_2018)
-        that is being prepared. If the working data already exists for that
-        profile, it will be loaded.
+        writing it (e.g. 'Whitelist.json' for class 'Whitelist'). The file
+        should be placed in a subfolder of the “savedata” directory,
+        corresponding to the vehicle profile (e.g. Ford_Fusion_2018).
+        The Rule abstract class "save_file" property is provided to establish
+        this.
+        The JSON should be a dictionary, with each key corresponding to the
+        class attribute name of the data being saved. For example:
+        {'whitelist': self.whitelist}.
 
         Args:
             canlist: default = None
             A list of CAN packets to analyze. If this argument is present, this
             function should analyze the data accordingly and save it,
             overwriting any existing data for that profile.
-            Else, the function should check for saved profile data.
+            Else, the function should attempt to load saved profile data.
         Raises:
-            If data required, but not found, raise a FileNotFoundError. If data
+            FileNotFoundError: If data required, but not found. If data
             not required, this method should not be implemented.
-
         """
         pass
+
+    def _load(self):
+        """Load saved profile state
+        This method loads data from a JSON file previously stored by
+        Rule.prepare
+        Note that the data contained in the JSON file will be unpacked into
+        attributes for this instance of Rule, to be used by the 'test' method
+        implemented by the child class.
+        Notes:
+            This allows for execution of arbitrary code
+            This method is marked as private, because it is intented to only be
+            used by child classes.
+        """
+        with self.save_file.open() as prof:
+            attr_dict = json.load(prof)
+        # unpack loaded JSON into class instance
+        for name, val in attr_dict.items():
+            setattr(self, name, val)
