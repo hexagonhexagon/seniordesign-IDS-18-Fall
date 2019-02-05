06a53ec9f795668ff79131b768b40daa4d1c6ec0
hexagonhexagon <hexagonhexagon163@gmail.com>
2019-02-05 17:18:21 -0500

    All functions initially outlined are done. May need more functions.

--- IDS/two_stage_ids.py
+++ IDS/two_stage_ids.py
@@ -1,36 +1,73 @@
-from dnn_ids import DNNBasedIDS
-import rules_ids as RuleBasedIDS
+from IDS.dnn_ids import DNNBasedIDS
+from IDS.rules_ids import RulesIDS
+import IDS.preprocessor as dp
 from numpy import log
 
 class TwoStageIDS:
     def __init__(self):
-        self.has_been_trained = False
+        self.dnn_trained = False
+        self.rules_trained = False
         self.in_simulation = False
-        self.params = {} # Should be initialized like the DNNBasedIDS.
+
         self.dnn = DNNBasedIDS()
-        self.idprobs = {} # This needs to be loaded from a file properly.
-                          # Possibly integrate into params.
+        self.rules = RulesIDS()
+        self.idprobs = {}
+
+        self.params = {
+            'dnn_dir_path': None,
+            'rules_dir_path': None,
+            'idprobs_path': None
+        }
         # Simulation variables start here.
         self.frames_last_sec = []
         self.idcounts = {}
         self.total_frames = 0
         self.system_entropy = 0
-        # Do more initialization here.
+
+    def load_ids(self):
+        if self.params['dnn_dir_path']:
+            self.dnn.load_model(self.params['dnn_dir_path'])
+            self.dnn_trained = True
+        if self.params['rules_dir_path']:
+            self.rules.prepare(set_profile_id=self.params['rules_dir_path'])
+            self.rules_trained = True
+        if self.params['idprobs_path']:
+            self.idprobs = dp.load_id_probs(self.params['idprobs_path'])
 
     def change_ids_parameters(self, key, value):
-        pass
+        if key not in self.params:
+            raise ValueError(f'{key} is not a valid parameter that can be changed. Valid parameters: {list(self.params.keys())}')
+        else:
+            self.params[key] = value
 
-    def retrain_on_dataset(self, input_function):
-        pass
+    def retrain_rules(self, canlist):
+        self.rules.prepare(canlist)
+        self.rules_trained = True
 
-    def judge_dataset(self, input_function):
-        pass
+    def retrain_dnn(self, input_function, num_steps):
+        if not self.dnn._dnn:
+            raise RuntimeError('No DNN has been initialized!')
+        self.dnn.train(input_function, num_steps)
+        self.dnn_trained = True
+
+    def judge_dataset(self, canlist, input_function):
+        if self.in_simulation:
+            raise RuntimeError('The TwoStageIDS must not be in a simulation in order to judge a dataset.')
+        rules_results = self.rules.test_series(canlist)
+        dnn_results = self.dnn.predict(input_function)
+        for rule_result, dnn_result in zip(rules_results, dnn_results):
+            if not rule_result[0]:
+                yield rule_result
+            else:
+                yield dnn_result
 
     def start_simulation(self):
         if self.in_simulation:
             raise RuntimeError('The TwoStageIDS has already begun a simulation!')
-        if not self.has_been_trained:
+        if not self.dnn_trained:
             raise RuntimeError('The DNN must be trained before a simulation can be started!')
+        if not self.rules_trained:
+            raise RuntimeError('The Rules Based IDS must be prepared before a simulation can be started!')
         self.frames_last_sec = []
         self.idcounts = {}
         self.total_frames = 0
@@ -43,7 +80,7 @@ class TwoStageIDS:
         self.in_simulation = False
 
     def judge_single_frame(self, frame):
-        is_malicious = RuleBasedIDS.test(frame)
+        is_malicious = self.rules.test(frame)
         if is_malicious[0]: # is_malicious is a pair (is_malicious, rule_name)
             return is_malicious
         else: # Passed RuleBasedIDS, test against DNNBasedIDS.
