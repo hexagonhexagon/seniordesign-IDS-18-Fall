a89c999c4a0c65defa6a39b97e18c26e08b06281
amaricq <amaricq@umich.edu>
2019-03-20 14:28:10 -0400

    Add setup.py for packaging
    Haven't tested
    Also, not really necessary at this point.

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='two-stage-ids',
+    version='0.1',
+    description='Two Stage Intrusion Detection for CAN bus',
+    long_description=long_description,
+    author='UM-Dearborn CIS Team 10 W19',
+
+    python_requires='>=3.6, <3.7',
+    install_requires=['numpy', 'tensorflow'],
+    extras_require={
+        'test': ['pytest']
+    },
+
+    packages=find_packages(exclude=['tests', 'old*', 'old', 'NNIDS*', 'NNIDS'])
+)
