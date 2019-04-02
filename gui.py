6570d4dadb046e82b28f14a3d60943e28965ac9b
hexagonhexagon <hexagonhexagon163@gmail.com>
2019-04-02 11:52:55 -0400

    Proof of Concept done in top level folder

--- gui.py
+++ gui.py
@@ -2,16 +2,19 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtQml import QQmlApplicationEngine
 
+from preprocessor_manager import DataPreprocessorManager
+
 def main():
     global app
     app = QApplication(sys.argv)
     app.setStyle('Fusion')
 
     engine = QQmlApplicationEngine()
+    dpmanager = DataPreprocessorManager()
+    engine.rootContext().setContextProperty("dpManager", dpmanager)
     engine.load('main.qml')
 
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
-
